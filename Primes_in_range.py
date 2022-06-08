46e5013410b95134fce0651b981b3543fc793132
Teja9703047204 <102518952+Teja9703047204@users.noreply.github.com>
2022-06-08 16:24:49 +0530

    def p(n):     if n==1:         return False     for i in range(2,int((n**0.5)+1)):         if n%i==0:             return False     else:         return True a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if p(i)==True:         c=c+1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,15 @@
+def p(n):
+    if n==1:
+        return False
+    for i in range(2,int((n**0.5)+1)):
+        if n%i==0:
+            return False
+    else:
+        return True
+a=int(input())
+b=int(input())
+c=0
+for i in range(a,b+1):
+    if p(i)==True:
+        c=c+1
+print(c)
